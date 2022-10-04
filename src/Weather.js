4737b71aadef0c32964aaeb1b3b24c81a8788598
Mariia-cioccolatini <107420122+Mariia-cioccolatini@users.noreply.github.com>
2022-10-04 17:05:30 +0200

    axios+api

--- src/Weather.js
+++ src/Weather.js
@@ -32,7 +32,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    const apiKey = "44c2d281c7701ddf7d1ccc23fa0b2ff9";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
